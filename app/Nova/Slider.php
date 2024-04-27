898568421b41a70da77a95179363cc21e067520a
saber tabatabaee yazdi <saber.tabatabaee@gmail.com>
2024-04-27 18:52:35 +0330

    menu action

---
+++ app/Nova/Slider.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Nova;
+
+use Faker\Provider\Text;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Slider extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Slider>
+     */
+    public static $model = \App\Models\Slider::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            \Laravel\Nova\Fields\Text::make('title'),
+            \Laravel\Nova\Fields\Text::make('description')->hideFromIndex(),
+            \Laravel\Nova\Fields\Text::make('image'),
+            \Laravel\Nova\Fields\Text::make('have_buttons'),
+            \Laravel\Nova\Fields\Text::make('enabled'),
+            \Laravel\Nova\Fields\Text::make('order'),
+//            \Laravel\Nova\Fields\Text::make('created_at'),
+//            \Laravel\Nova\Fields\Text::make('updated_at'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
