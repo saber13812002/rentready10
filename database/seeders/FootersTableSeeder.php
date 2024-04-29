447fc2dced3d0a8cfef6c64cc95c1732b860dd6c
saber tabatabaee yazdi <saber.tabatabaee@gmail.com>
2024-04-29 18:06:38 +0330

    footer hide become pro link

--- database/seeders/FootersTableSeeder.php
+++ database/seeders/FootersTableSeeder.php
@@ -31,7 +31,7 @@ class FootersTableSeeder extends Seeder
                 'terms_show' => 1,
                 'privacy_show' => 1,
                 'contact_us_show' => 1,
-                'become_pro_show' => 1,
+                'become_pro_show' => 0,
                 'become_pro_link' => 'https://forms.office.com/pages/responsepage.aspx?id=qwYkC7-7nkuVok9d2X9z69ifxk1djudFjSJPDr2zyIdUNVIxWjcyTjdGRlM5RkwzWDBMRlcwR0hIMC4u/.',
                 'created_at' => '2024-04-28 12:28:06',
                 'updated_at' => '2024-04-28 12:28:09',
