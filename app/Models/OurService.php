b5185428db1d649132ae92de0c7a90818673159d
saber <saber.tabatabaee@gmail.com>
2024-05-11 19:11:13 +0330

    our service single logo upload

---
+++ app/Models/OurService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class OurService extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(130)
+            ->height(130);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('our-service-logo')->singleFile();
+    }
+}
---
+++ app/Nova/OurService.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Nova;
+
+use Ebess\AdvancedNovaMediaLibrary\Fields\Images;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class OurService extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\OurService>
+     */
+    public static $model = \App\Models\OurService::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Images::make('logo' , 'our-service-logo')
+                ->conversionOnIndexView('thumb') // conversion used to display the image
+                ->rules('required'),
+
+            Text::make('title'),
+            Boolean::make('enabled'),
+            Number::make('order'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
