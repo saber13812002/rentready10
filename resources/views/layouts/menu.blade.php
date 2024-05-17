7c36fa23872a63a26b6edaeb1e7776d8b0cd0129
saber <saber.tabatabaee@gmail.com>
2024-05-17 23:32:16 +0330

    new wg taha

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,11 +1,40 @@
-
-
-@foreach($menus as $menu)
-
-    <div class="header-nav-item header-nav-item--collection header-nav-item--active">
-        <a href="/{{ $menu->slug }}" data-animation-role="header-element" aria-current="page" class="preSlide slideIn" style="transition-timing-function: ease; transition-duration: 0.45s; transition-delay: 0.0104348s;">
-            {{$menu->title}}
-        </a>
+<!-- Navbar Area -->
+<div class="navbar-area relative z-2">
+    <div class="wg-responsive-nav lg:hidden">
+        <div class="container">
+            <div class="wg-responsive-menu">
+                <div class="logo">
+                    <a href="/">
+                        <img src="{{ $footers->logo }}" alt="logo" class="w-[90px] h-[90px]">
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="wg-nav hidden lg:block">
+        <div class="container xl:max-w-full 3xl:max-w-[1700px]">
+            <nav class="navbar flex flex-row flex-wrap pt-10 pb-10">
+                <a class="navbar-brand self-center w-[90px] h-[90px]" href="/">
+                    <img src="{{ $footers->logo }}" alt="logo">
+                </a>
+                <div class="flex self-center grow basis-auto gap-18">
+                    <ul class="self-center flex flex-row gap-18 ml-auto pt-30 pb-30">
 
-@endforeach
+                        @foreach($menus as $menu)
+                            <li class="mr-12 xl:mr-15 relative group">
+                                <a href="/{{ $menu->slug }}"
+                                   class="block text-black-color active font-semibold text-16px ease-in duration-300 relative">{{$menu->title}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                    <div class="self-center">
+                        <a href="contact.html"
+                           class="inline-block font-semibold text-13px md:text-14px lg:text-15px rounded-xl text-white pt-17 pb-13 pl-35 pr-35 bg-[#14944c] shadow-custom-box-shadow hover:shadow-secondary-btn ease-in duration-300">Login</a>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    </div>
+</div>
+<!-- End Navbar Area -->
