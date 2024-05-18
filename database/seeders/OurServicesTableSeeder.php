ae3eef73fe7f80d1c9da30a2fc8dafaf41a581dd
saber <saber.tabatabaee@gmail.com>
2024-05-19 01:21:36 +0330

    OurServicesTableSeeder.php

--- database/seeders/OurServicesTableSeeder.php
+++ database/seeders/OurServicesTableSeeder.php
@@ -23,9 +23,9 @@ class OurServicesTableSeeder extends Seeder
             array (
                 'id' => 1,
                 'title' => 'Paint',
+                'odd' => 1,
                 'enabled' => 1,
                 'order' => 1,
-                'odd' => 1,
                 'created_at' => '2024-05-11 19:12:06',
                 'updated_at' => '2024-05-11 19:12:06',
             ),
@@ -33,9 +33,9 @@ class OurServicesTableSeeder extends Seeder
             array (
                 'id' => 2,
                 'title' => 'Clean',
+                'odd' => 1,
                 'enabled' => 1,
                 'order' => 2,
-                'odd' => 1,
                 'created_at' => '2024-05-11 19:12:33',
                 'updated_at' => '2024-05-11 19:12:33',
             ),
@@ -43,9 +43,9 @@ class OurServicesTableSeeder extends Seeder
             array (
                 'id' => 3,
                 'title' => 'Carpet Cleaning',
+                'odd' => 1,
                 'enabled' => 1,
                 'order' => 3,
-                'odd' => 1,
                 'created_at' => '2024-05-11 19:14:07',
                 'updated_at' => '2024-05-11 19:14:07',
             ),
@@ -53,9 +53,49 @@ class OurServicesTableSeeder extends Seeder
             array (
                 'id' => 4,
                 'title' => 'Maintenace',
+                'odd' => 1,
                 'enabled' => 1,
                 'order' => 4,
-                'odd' => 1,
+                'created_at' => '2024-05-11 19:15:31',
+                'updated_at' => '2024-05-11 19:15:31',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'title' => 'Paint2',
+                'odd' => 0,
+                'enabled' => 1,
+                'order' => 1,
+                'created_at' => '2024-05-11 19:12:06',
+                'updated_at' => '2024-05-11 19:12:06',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'title' => 'Clean3',
+                'odd' => 0,
+                'enabled' => 1,
+                'order' => 2,
+                'created_at' => '2024-05-11 19:12:33',
+                'updated_at' => '2024-05-11 19:12:33',
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'title' => 'Carpet Cleaning4',
+                'odd' => 0,
+                'enabled' => 1,
+                'order' => 3,
+                'created_at' => '2024-05-11 19:14:07',
+                'updated_at' => '2024-05-11 19:14:07',
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'title' => 'Maintenace5',
+                'odd' => 0,
+                'enabled' => 1,
+                'order' => 4,
                 'created_at' => '2024-05-11 19:15:31',
                 'updated_at' => '2024-05-11 19:15:31',
             ),
