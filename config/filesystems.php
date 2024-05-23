183d201079e03e29a747f3bfa990ad238b4bbe06
saber <saber.tabatabaee@gmail.com>
2024-05-24 01:18:49 +0330

    'root' => env('STORAGE_PATH', storage_path('app/public')),

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => env('STORAGE_PATH', storage_path('app/public')),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
